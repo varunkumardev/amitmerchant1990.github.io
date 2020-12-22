2d9bb2e515807282c8b41dac5f61343d4a54ce41
amitmerchant1990 <bullredeyes@gmail.com>
2020-12-22 17:23:55 +0530

    implement subtle vibration on tapping the hamburger menu.

---
+++ js/app.js
@@ -0,0 +1,14 @@
+window.addEventListener('keypress', function (e) {
+    // Navigates to search 
+    // page on pressing "/"
+    if (e.keyCode === 47) {
+        window.location.href = '/search';
+    }
+}, false);
+
+// vibrate the device when tapping on the hamburger menu
+let menuBtn = document.getElementById('menu-btn');
+
+menuBtn.addEventListener('click', function () {
+    window.navigator.vibrate(50);
+});
