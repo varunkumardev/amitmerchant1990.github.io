d06699a8439bc88cd01805ccccd7cfe1536d3222
amitmerchant1990 <bullredeyes@gmail.com>
2021-08-12 20:13:02 +0530

    implement button to copy code blocks.

--- js/app.js
+++ js/app.js
@@ -21,3 +21,27 @@ let menuBtn = document.getElementById('menu-btn');
 menuBtn.addEventListener('click', function () {
     window.navigator.vibrate(50);
 });
+
+const codeBlocks = document.querySelectorAll('pre.highlight');
+
+codeBlocks.forEach(function (codeBlock) {
+  let copyButton = document.createElement('button');
+  copyButton.className = 'copy';
+  copyButton.type = 'button';
+  copyButton.ariaLabel = 'Copy code to clipboard';
+  copyButton.innerText = 'Copy';
+
+  codeBlock.append(copyButton);
+
+  copyButton.addEventListener('click', function () {
+    let code = codeBlock.querySelector('code').innerText.trim();
+    window.navigator.clipboard.writeText(code);
+
+    copyButton.innerText = 'Copied';
+    let twoSeconds = 2000;
+
+    setTimeout(function () {
+      copyButton.innerText = 'Copy';
+    }, twoSeconds);
+  });
+});
